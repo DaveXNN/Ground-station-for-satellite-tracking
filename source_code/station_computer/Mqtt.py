c37620f1e81fbf0c83f35076ea1bf86ec077a505
David Nenicka <122628660+DaveXNN@users.noreply.github.com>
2024-08-05 20:59:56 +0200

    Update Mqtt.py

--- source_code/station_computer/Mqtt.py
+++ source_code/station_computer/Mqtt.py
@@ -1,47 +1,54 @@
 import paho.mqtt.client as paho                                     # for subscribing data from MQTT broker
 
-from datetime import datetime
-from threading import Thread, Timer
-
-
-def print_info(msg):
-    print('{0} UTC, {1}'.format(datetime.utcnow(), msg))
+from datetime import datetime, timezone
+from socket import gaierror
+from threading import Timer
 
 
 class Mqtt:
     def __init__(self):
         self.client = paho.Client()                                 # create mqtt client
-        self.client.username_pw_set(<username>, password=<password>)    # mqtt server authorization
+        self.client.username_pw_set('laptop', password='laptop')    # mqtt server authorization
+        self.connected = False
         self.az = '0.00'
         self.el = '0.00'
-        Thread(target=self.try_connect).start()
+        self.enabled = True
+        self.try_connect()
+
+    @staticmethod
+    def print_info(msg):
+        print(f'{datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")}, {msg}')
 
     def try_connect(self):
         try:
-            self.client.connect(<hostname>, port=<port>)
+            self.client.connect('raspberrypi', port=1883)
             self.client.on_connect = self.on_connect
             self.client.on_message = self.on_message
             self.client.subscribe('azimuth')
             self.client.subscribe('elevation')
             self.client.loop_start()
-        except:
-            print_info('laptop could not connect to MQTT Broker')
-            Timer(60, self.try_connect).start()
+        except gaierror:
+            self.print_info('laptop could not connect to MQTT Broker')
+            if self.enabled:
+                self.connect_thread = Timer(30, self.try_connect)
+                self.connect_thread.start()
 
-    @staticmethod
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
-            print_info('laptop connected to MQTT broker')
+            self.print_info('laptop connected to MQTT broker')
+            self.connected = True
         else:
-            print_info('laptop could not connect to MQTT broker' + str(rc))
+            self.print_info('laptop could not connect to MQTT broker' + str(rc))
+            self.connected = False
 
     def on_message(self, client, userdata, message):
         topic = str(message.topic)
-        msg = str(round(float(str(message.payload.decode('utf-8'))), 2))
+        msg = float(str(message.payload.decode('utf-8')))
+        msg2 = f'{msg:.2f}'
         if topic == 'azimuth':
-            self.az = msg
+            self.az = msg2
         if topic == 'elevation':
-            self.el = msg
+            self.el = msg2
 
     def publish_data(self, d_time, d_azimuth, d_elevation):
         self.client.publish('delta_time', str(d_time), 0)
@@ -53,11 +60,6 @@ class Mqtt:
 
     def publish_action(self, action):
         self.client.publish('action', action, 0)
-        print_info('published action: {0}'.format(action))
 
     def publish_polarization(self, pol):
         self.client.publish('polarization', pol, 0)
-        print_info('polarization changed to: {0}'.format(pol))
-
-
-mqtt = Mqtt()
