c15031f3d788e9d9aa1516fc53a678d3a1c2054b
David Nenicka <122628660+DaveXNN@users.noreply.github.com>
2024-08-23 11:42:06 +0200

    Update Publisher.py

--- source_code/rotator/Publisher.py
+++ source_code/rotator/Publisher.py
@@ -8,8 +8,8 @@ class Publisher:
         self.client = paho.Client()                                 # create mqtt client
         self.az = az
         self.el = el
-        self.client.username_pw_set('azimuthstepper', password='azimuthstepper')  # mqtt server authorization
-        if self.client.connect('raspberrypi', port=1883) == 0:
+        self.client.username_pw_set(<username>, password=<password>)# mqtt server authorization
+        if self.client.connect(<hostname>, port=<port>) == 0:
             print('Publisher connected to MQTT Broker', sep=', ')
         else:
             print('Publisher could not connect to MQTT Broker', sep=', ')
