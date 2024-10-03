3018de62ecfc573e9e1b5bbb0049828c7b46d061
David Nenicka <122628660+DaveXNN@users.noreply.github.com>
2024-10-03 12:51:08 +0200

    Add files via upload

--- source_code/station_computer/StationComputer.py
+++ source_code/station_computer/StationComputer.py
@@ -1,3 +1,13 @@
+########################################################################################################################
+#                                                                                                                      #
+#    Author:         D. Nenicka                                                                                        #
+#    Created:        3. 11. 2023                                                                                       #
+#    Modified:       30. 8. 2024                                                                                       #
+#    Description:    The main script of Satellite Tracking Software                                                    #
+#                                                                                                                      #
+########################################################################################################################
+
+
 from BeyondTools import BeyondTools                             # module for predicting satellite visibility
 from Mqtt import Mqtt                                           # module for MQTT communication
 from TleUpdator import TleUpdator                               # module for downloading TLE data
